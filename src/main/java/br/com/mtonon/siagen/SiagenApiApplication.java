5102ff5e44c8875aed166fcfcfc53bcfde89f7e4
mbettcher <mtonon.pmg@gmail.com>
2021-10-12 00:45:48 -0300

    Projeto criado

---
+++ src/main/java/br/com/mtonon/siagen/SiagenApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.siagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiagenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiagenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/mtonon/siagen/SiagenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mtonon.siagen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiagenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
