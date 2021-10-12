fa3c6d13afef0c2b4a0e165215e635defa487523
mbettcher <mtonon.pmg@gmail.com>
2021-10-12 16:30:52 -0300

    Banco de Dados H2 e criação automáticva da base de dados

--- src/main/java/br/com/mtonon/siagen/domain/Especialidade.java
+++ src/main/java/br/com/mtonon/siagen/domain/Especialidade.java
@@ -1,18 +1,29 @@
 package br.com.mtonon.siagen.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-
+@Entity
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Especialidade {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "esp_codigo")
 	private Long id;
+	
+	@Column(name = "esp_descricao", nullable = false, unique = true)
 	private String descricao;
 	
 }
