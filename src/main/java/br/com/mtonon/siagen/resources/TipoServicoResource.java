64603db382d55fdb90b2a60245a837182823e9b9
mbettcher <mtonon.pmg@gmail.com>
2021-10-19 18:08:31 -0300

    Listando todos os TipoServico com DTO

--- src/main/java/br/com/mtonon/siagen/resources/TipoServicoResource.java
+++ src/main/java/br/com/mtonon/siagen/resources/TipoServicoResource.java
@@ -2,6 +2,7 @@ package br.com.mtonon.siagen.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mtonon.siagen.domain.TipoServico;
+import br.com.mtonon.siagen.dto.TipoServicoDTO;
 import br.com.mtonon.siagen.services.TipoServicoService;
 
 @RestController
@@ -23,9 +25,10 @@ public class TipoServicoResource {
 	private TipoServicoService tipoServicoService;
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<?> findAll() {
-		List<TipoServico> obj = tipoServicoService.findAll();
-		return ResponseEntity.ok().body(obj);
+	public ResponseEntity<List<TipoServicoDTO>> findAll() {
+		List<TipoServico> list = tipoServicoService.findAll();
+		List<TipoServicoDTO> listDTO = list.stream().map(obj -> new TipoServicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
