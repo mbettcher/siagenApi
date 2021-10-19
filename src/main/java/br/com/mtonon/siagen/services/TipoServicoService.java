5f6bc03e2ca42341aabd0ad03182f0764602f329
mbettcher <mtonon.pmg@gmail.com>
2021-10-19 19:02:59 -0300

    Paginacao com parâmetros opcionais na requisição para Tipo Serviço

--- src/main/java/br/com/mtonon/siagen/services/TipoServicoService.java
+++ src/main/java/br/com/mtonon/siagen/services/TipoServicoService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.mtonon.siagen.domain.TipoServico;
@@ -49,6 +52,11 @@ public class TipoServicoService {
 					"Não é possível excluir um Tipo de Serviço que esteja sendo usado por outra tabela!"
 					);
 		}
+	}
+	
+	public Page<TipoServico> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return tipoServicoRepository.findAll(pageRequest);
 	}
 
 }
