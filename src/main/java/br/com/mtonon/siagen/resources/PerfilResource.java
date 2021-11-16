677f06341ac4d648947912fec61976efa6c75f19
mbettcher <mtonon.pmg@gmail.com>
2021-11-15 23:25:05 -0300

    "Criado PerfilDTO e CRUD para PerfilService e PerfilResource"

--- src/main/java/br/com/mtonon/siagen/resources/PerfilResource.java
+++ src/main/java/br/com/mtonon/siagen/resources/PerfilResource.java
@@ -1,16 +1,24 @@
 package br.com.mtonon.siagen.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.mtonon.siagen.domain.Perfil;
+import br.com.mtonon.siagen.dto.PerfilDTO;
 import br.com.mtonon.siagen.services.PerfilService;
 
 @RestController
@@ -22,14 +30,47 @@ public class PerfilResource {
 	
 	@GetMapping
 	public ResponseEntity<?> listar() {
-		List<Perfil> obj = perfilService.listar();
+		List<Perfil> obj = perfilService.findAll();
 		return ResponseEntity.ok().body(obj);
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> buscar(@PathVariable Integer id) {
-		Perfil obj = perfilService.buscar(id);
+		Perfil obj = perfilService.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<PerfilDTO>> findPage(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "perfil") String orderBy){
+		Page<Perfil> list = perfilService.findPage(page, linesPerPage, direction, orderBy);
+		Page<PerfilDTO> listDTO = list.map(obj -> new PerfilDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<?> save(@RequestBody @Valid PerfilDTO objDTO) {
+		Perfil obj = perfilService.fromDTO(objDTO);
+		obj = perfilService.save(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<?> update(@RequestBody @Valid PerfilDTO objDTO, @PathVariable Integer id) {
+		Perfil obj = perfilService.fromDTO(objDTO);
+		obj.setId(id);
+		obj = perfilService.update(obj);
+		return ResponseEntity.noContent().build();
+		}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<?> delete(@PathVariable Integer id) {
+		perfilService.delete(id);
+		return ResponseEntity.noContent().build();
+		}
 }
--- src/main/java/br/com/mtonon/siagen/services/PerfilService.java
+++ src/main/java/br/com/mtonon/siagen/services/PerfilService.java
@@ -4,10 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.mtonon.siagen.domain.Perfil;
+import br.com.mtonon.siagen.dto.PerfilDTO;
 import br.com.mtonon.siagen.repositories.PerfilRepository;
+import br.com.mtonon.siagen.services.exceptions.DataIntegrityException;
 import br.com.mtonon.siagen.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -16,15 +22,45 @@ public class PerfilService {
 	@Autowired
 	private PerfilRepository perfilRepository;
 	
-	public List<Perfil> listar() {
+	public List<Perfil> findAll() {
 		List<Perfil> obj = perfilRepository.findAll();
 		return obj;
 	}
 	
-	public Perfil buscar(Integer id) {
+	public Perfil find(Integer id) {
 		Optional<Perfil> obj = perfilRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Perfil.class.getName()));
 	}
+	
+	public Page<Perfil> findPage(Integer page, Integer linesPerPage, String direction, String orderBy) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return perfilRepository.findAll(pageRequest);
+	}
 
+	public Perfil save(Perfil obj) {
+		obj.setId(null);
+		return perfilRepository.save(obj);
+	}
+	
+	public Perfil update(Perfil obj) {
+		find(obj.getId());
+		return perfilRepository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			perfilRepository.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException(
+					"Não é possível excluir um perfil que esteja sendo usado por outra tabela!"
+					);
+		}
+		
+	}
+	
+	public Perfil fromDTO(PerfilDTO objDTO) {
+		return new Perfil(objDTO.getId(), objDTO.getPerfil());
+	}
 }
