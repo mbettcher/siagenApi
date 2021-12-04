824efb30aa24244912cbf71d8f607de4422dba30
mbettcher <mtonon.pmg@gmail.com>
2021-12-04 17:30:28 -0300

    Refinado método Save em UnidadeSaudeService e UnidadeSaudeNewDTO

--- src/main/java/br/com/mtonon/siagen/dto/UnidadeSaudeNewDTO.java
+++ src/main/java/br/com/mtonon/siagen/dto/UnidadeSaudeNewDTO.java
@@ -1,66 +1,39 @@
 package br.com.mtonon.siagen.dto;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
-public class UnidadeSaudeNewDTO implements Serializable{
+import br.com.mtonon.siagen.domain.DiaSemana;
+import br.com.mtonon.siagen.domain.Especialidade;
+import br.com.mtonon.siagen.domain.Servico;
+
+public class UnidadeSaudeNewDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private Integer id;
+
 	private String nome;
 	private Boolean ativo;
-	
+
 	@NotBlank(message = "O campo telefone é obrigatório!")
 	private String telefone1;
 	private String telefone2;
 	private String telefone3;
-	
-	@NotNull(message = "O campo especialidade é obrigatório!")
-	private Integer especialidade1;
-	private Integer especialidade2;
-	private Integer especialidade3;
-	private Integer especialidade4;
-	
+
 	private String logradouro;
 	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
 	private Integer cidadeId;
-	
-	@NotNull(message = "O campo Serviço é obrigatório!")
-	private Integer servico1;
-	private Integer servico2;
-	private Integer servico3;
-	private Integer servico4;
-	private Integer servico5;
-	private Integer servico6;
-	private Integer servico7;
-	private Integer servico8;
-	private Integer servico9;
-	private Integer servico10;
-	
-	@NotNull(message = "O campo Dia de Funcionamento é obrigatório!")
-	private Integer diaFuncionamento1;
-	private Integer diaFuncionamento2;
-	private Integer diaFuncionamento3;
-	private Integer diaFuncionamento4;
-	private Integer diaFuncionamento5;
-	private Integer diaFuncionamento6;
-	private Integer diaFuncionamento7;
-	
-	
+
+	private List<Especialidade> especialidades;
+
+	private List<Servico> servicos;
+
+	private List<DiaSemana> diasFuncionamento;
+
 	public UnidadeSaudeNewDTO() {
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
 	}
 
 	public String getNome() {
@@ -101,38 +74,6 @@ public class UnidadeSaudeNewDTO implements Serializable{
 
 	public void setTelefone3(String telefone3) {
 		this.telefone3 = telefone3;
-	}
-
-	public Integer getEspecialidade1() {
-		return especialidade1;
-	}
-
-	public void setEspecialidade1(Integer especialidade1) {
-		this.especialidade1 = especialidade1;
-	}
-
-	public Integer getEspecialidade2() {
-		return especialidade2;
-	}
-
-	public void setEspecialidade2(Integer especialidade2) {
-		this.especialidade2 = especialidade2;
-	}
-
-	public Integer getEspecialidade3() {
-		return especialidade3;
-	}
-
-	public void setEspecialidade3(Integer especialidade3) {
-		this.especialidade3 = especialidade3;
-	}
-
-	public Integer getEspecialidade4() {
-		return especialidade4;
-	}
-
-	public void setEspecialidade4(Integer especialidade4) {
-		this.especialidade4 = especialidade4;
 	}
 
 	public String getLogradouro() {
@@ -183,139 +124,27 @@ public class UnidadeSaudeNewDTO implements Serializable{
 		this.cidadeId = cidadeId;
 	}
 
-	public Integer getServico1() {
-		return servico1;
+	public List<Especialidade> getEspecialidades() {
+		return especialidades;
 	}
 
-	public void setServico1(Integer servico1) {
-		this.servico1 = servico1;
+	public void setEspecialidades(List<Especialidade> especialidades) {
+		this.especialidades = especialidades;
 	}
 
-	public Integer getServico2() {
-		return servico2;
+	public List<Servico> getServicos() {
+		return servicos;
 	}
 
-	public void setServico2(Integer servico2) {
-		this.servico2 = servico2;
+	public void setServicos(List<Servico> servicos) {
+		this.servicos = servicos;
 	}
 
-	public Integer getServico3() {
-		return servico3;
+	public List<DiaSemana> getDiasFuncionamento() {
+		return diasFuncionamento;
 	}
 
-	public void setServico3(Integer servico3) {
-		this.servico3 = servico3;
-	}
-
-	public Integer getServico4() {
-		return servico4;
-	}
-
-	public void setServico4(Integer servico4) {
-		this.servico4 = servico4;
-	}
-
-	public Integer getServico5() {
-		return servico5;
-	}
-
-	public void setServico5(Integer servico5) {
-		this.servico5 = servico5;
-	}
-
-	public Integer getServico6() {
-		return servico6;
-	}
-
-	public void setServico6(Integer servico6) {
-		this.servico6 = servico6;
-	}
-
-	public Integer getServico7() {
-		return servico7;
-	}
-
-	public void setServico7(Integer servico7) {
-		this.servico7 = servico7;
-	}
-
-	public Integer getServico8() {
-		return servico8;
-	}
-
-	public void setServico8(Integer servico8) {
-		this.servico8 = servico8;
-	}
-
-	public Integer getServico9() {
-		return servico9;
-	}
-
-	public void setServico9(Integer servico9) {
-		this.servico9 = servico9;
-	}
-
-	public Integer getServico10() {
-		return servico10;
-	}
-
-	public void setServico10(Integer servico10) {
-		this.servico10 = servico10;
-	}
-
-	public Integer getDiaFuncionamento1() {
-		return diaFuncionamento1;
-	}
-
-	public void setDiaFuncionamento1(Integer diaFuncionamento1) {
-		this.diaFuncionamento1 = diaFuncionamento1;
-	}
-
-	public Integer getDiaFuncionamento2() {
-		return diaFuncionamento2;
-	}
-
-	public void setDiaFuncionamento2(Integer diaFuncionamento2) {
-		this.diaFuncionamento2 = diaFuncionamento2;
-	}
-
-	public Integer getDiaFuncionamento3() {
-		return diaFuncionamento3;
-	}
-
-	public void setDiaFuncionamento3(Integer diaFuncionamento3) {
-		this.diaFuncionamento3 = diaFuncionamento3;
-	}
-
-	public Integer getDiaFuncionamento4() {
-		return diaFuncionamento4;
-	}
-
-	public void setDiaFuncionamento4(Integer diaFuncionamento4) {
-		this.diaFuncionamento4 = diaFuncionamento4;
-	}
-
-	public Integer getDiaFuncionamento5() {
-		return diaFuncionamento5;
-	}
-
-	public void setDiaFuncionamento5(Integer diaFuncionamento5) {
-		this.diaFuncionamento5 = diaFuncionamento5;
-	}
-
-	public Integer getDiaFuncionamento6() {
-		return diaFuncionamento6;
-	}
-
-	public void setDiaFuncionamento6(Integer diaFuncionamento6) {
-		this.diaFuncionamento6 = diaFuncionamento6;
-	}
-
-	public Integer getDiaFuncionamento7() {
-		return diaFuncionamento7;
-	}
-
-	public void setDiaFuncionamento7(Integer diaFuncionamento7) {
-		this.diaFuncionamento7 = diaFuncionamento7;
+	public void setDiasFuncionamento(List<DiaSemana> diasFuncionamento) {
+		this.diasFuncionamento = diasFuncionamento;
 	}
 }
--- src/main/java/br/com/mtonon/siagen/services/UnidadeSaudeService.java
+++ src/main/java/br/com/mtonon/siagen/services/UnidadeSaudeService.java
@@ -1,6 +1,7 @@
 package br.com.mtonon.siagen.services;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,12 +14,11 @@ import br.com.mtonon.siagen.domain.Cidade;
 import br.com.mtonon.siagen.domain.DiaSemana;
 import br.com.mtonon.siagen.domain.EnderecoUnidadeSaude;
 import br.com.mtonon.siagen.domain.Especialidade;
+import br.com.mtonon.siagen.domain.Servico;
 import br.com.mtonon.siagen.domain.UnidadeSaude;
 import br.com.mtonon.siagen.dto.UnidadeSaudeDTO;
 import br.com.mtonon.siagen.dto.UnidadeSaudeNewDTO;
-import br.com.mtonon.siagen.repositories.DiaSemanaRepository;
 import br.com.mtonon.siagen.repositories.EnderecoUnidadeSaudeRepository;
-import br.com.mtonon.siagen.repositories.EspecialidadeRepository;
 import br.com.mtonon.siagen.repositories.UnidadeSaudeRepository;
 import br.com.mtonon.siagen.services.exceptions.DataIntegrityException;
 import br.com.mtonon.siagen.services.exceptions.ObjectNotFoundException;
@@ -28,15 +28,10 @@ public class UnidadeSaudeService {
 
 	@Autowired
 	private UnidadeSaudeRepository unidadeSaudeRepository;
-	
-	@Autowired
-	private EspecialidadeRepository especialidadeRepository;
-	
+
 	@Autowired
 	private EnderecoUnidadeSaudeRepository enderecoUnidadeSaudeRepository;
-	
-	@Autowired
-	private DiaSemanaRepository diaSemanaRepository;
+
 
 	public UnidadeSaude findById(Integer id) {
 		Optional<UnidadeSaude> obj = unidadeSaudeRepository.findById(id);
@@ -52,9 +47,20 @@ public class UnidadeSaudeService {
 	@Transactional
 	public UnidadeSaude save(UnidadeSaude obj) {
 		obj.setId(null);
-		//especialidadeRepository.saveAll(obj.getEspecialidades());
 		enderecoUnidadeSaudeRepository.saveAll(obj.getEnderecos());
-		//diaSemanaRepository.saveAll(obj.getDiasFuncionamento());
+		
+		for(Especialidade esp : obj.getEspecialidades()) {
+			esp.setUnidadesSaude(Arrays.asList(obj));
+		}
+		
+		for(Servico serv : obj.getServicos()) {
+			serv.setUnidadesSaude(Arrays.asList(obj));
+		}
+		
+		for(DiaSemana ds : obj.getDiasFuncionamento()) {
+			ds.setUnidadesSaude(Arrays.asList(obj));
+		}
+		
 		return unidadeSaudeRepository.save(obj);
 	}
 
@@ -92,75 +98,17 @@ public class UnidadeSaudeService {
 			unidadeSaude.getTelefones().add(objDTO.getTelefone3());
 		}
 
-		/*
-		Especialidade especialidade1 = new Especialidade(objDTO.getEspecialidade1(), null);
-		especialidade1.getUnidadesSaude().add(unidadeSaude);
-		unidadeSaude.getEspecialidades().add(especialidade1);
-		
-
-		if (objDTO.getEspecialidade2() != null) {
-			Especialidade especialidade2 = new Especialidade(objDTO.getEspecialidade2(), null);
-			unidadeSaude.getEspecialidades().add(especialidade2);
-			especialidade2.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getEspecialidade3() != null) {
-			Especialidade especialidade3 = new Especialidade(objDTO.getEspecialidade3(), null);
-			unidadeSaude.getEspecialidades().add(especialidade3);
-			especialidade3.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getEspecialidade4() != null) {
-			Especialidade especialidade4 = new Especialidade(objDTO.getEspecialidade4(), null);
-			unidadeSaude.getEspecialidades().add(especialidade4);
-			especialidade4.getUnidadesSaude().add(unidadeSaude);
-		}
-		*/ 
-
 		Cidade cidade = new Cidade(objDTO.getCidadeId(), null, null, null, null);
-
 		EnderecoUnidadeSaude endereco = new EnderecoUnidadeSaude(null, objDTO.getLogradouro(), objDTO.getNumero(),
 				objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cidade, unidadeSaude);
 
 		unidadeSaude.getEnderecos().add(endereco);
 		endereco.setUnidadeSaude(unidadeSaude);
-
-		/*
-		DiaSemana diaFuncionamento1 = new DiaSemana(objDTO.getDiaFuncionamento1(), null, false, null);
-		unidadeSaude.getDiasFuncionamento().add(diaFuncionamento1);
-		diaFuncionamento1.getUnidadesSaude().add(unidadeSaude);
-
-		if (objDTO.getDiaFuncionamento2() != null) {
-			DiaSemana diaFuncionamento2 = new DiaSemana(objDTO.getDiaFuncionamento2(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento2);
-			diaFuncionamento2.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getDiaFuncionamento3() != null) {
-			DiaSemana diaFuncionamento3 = new DiaSemana(objDTO.getDiaFuncionamento3(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento3);
-			diaFuncionamento3.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getDiaFuncionamento4() != null) {
-			DiaSemana diaFuncionamento4 = new DiaSemana(objDTO.getDiaFuncionamento4(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento4);
-			diaFuncionamento4.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getDiaFuncionamento5() != null) {
-			DiaSemana diaFuncionamento5 = new DiaSemana(objDTO.getDiaFuncionamento5(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento5);
-			diaFuncionamento5.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getDiaFuncionamento6() != null) {
-			DiaSemana diaFuncionamento6 = new DiaSemana(objDTO.getDiaFuncionamento6(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento6);
-			diaFuncionamento6.getUnidadesSaude().add(unidadeSaude);
-		}
-		if (objDTO.getDiaFuncionamento7() != null) {
-			DiaSemana diaFuncionamento7 = new DiaSemana(objDTO.getDiaFuncionamento7(), null, false, null);
-			unidadeSaude.getDiasFuncionamento().add(diaFuncionamento7);
-			diaFuncionamento7.getUnidadesSaude().add(unidadeSaude);
-		}
-		*/
-
 		
+		unidadeSaude.setEspecialidades(objDTO.getEspecialidades());
+		unidadeSaude.setServicos(objDTO.getServicos());
+		unidadeSaude.setDiasFuncionamento(objDTO.getDiasFuncionamento());
+
 		return unidadeSaude;
 	}
 
