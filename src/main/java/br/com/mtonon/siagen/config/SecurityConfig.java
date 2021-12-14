98c4fd2765da22ef0db2e3a3a4c6d9aa4ea510ed
mbettcher <mtonon.pmg@gmail.com>
2021-12-14 16:57:52 -0300

    Modificado PUBLIC_MATCHERS do arquivo SecurityConfig

--- src/main/java/br/com/mtonon/siagen/config/SecurityConfig.java
+++ src/main/java/br/com/mtonon/siagen/config/SecurityConfig.java
@@ -23,7 +23,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
 	private static final String[] PUBLIC_MATCHERS = {
 			"/h2-console/**",
-			"/especialidades/**"
+			"/especialidades/**",
+			"/dias/horarios/**"
 	};
 	
 	@Override
